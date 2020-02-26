df191071b834b4338750d5ecebf0b4b187d04f60
Hellowalls1 <thesingingbutcher@gmail.com>
2020-02-26 14:24:15 -0600

    switching to master

---
+++ script/FishDataProvider.js
@@ -0,0 +1,26 @@
+const fishCollection = [
+{
+    name: "Reginald",
+    species: "goldfish",
+    length: "6 cm",
+    harvested: "Hawaii",
+    eats: ["Flakes"]
+},
+{
+    name: "Geraldine",
+    species: "shark",
+    length: "1 ft",
+    harvested: "Florida",
+    eats: ["legs", "fish"]
+},
+{
+    name: "Francois",
+    species: "blue gill",
+    length: "8 cm",
+    harvested: "Apartment Complex",
+    eats: ["worms"]
+},
+
+]
+export const useFish = () => {
+    return fishCollection
---
+++ script/main.js
@@ -0,0 +1,8 @@
+import initializeDetailButtonEvents from '.dialog.js'
+import { useFish } from './FishDataProvider.js'
+
+initializeDetailButtonEvents()
+
+const arrayOfTheFishObjects = useFish()
+
+for ()
